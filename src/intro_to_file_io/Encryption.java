65b867b6bc6f5dd2a1c9a0335daf9712a69afe70
honeybiscuit15 <league@WTS14.attlocal.net>
2018-02-11 17:31:47 -0800

    made very little progress

--- src/intro_to_file_io/Encryption.java
+++ src/intro_to_file_io/Encryption.java
@@ -5,30 +5,29 @@ import javax.swing.JOptionPane;
 public class Encryption {
 
 	public static void main(String[] args) {
-		
+
 		letsEncryptSomething();
 	}
-	
-	static void letsEncryptSomething()
-	{
-		String word = "David";
-		char[]name = word.toCharArray();
-		for (int i = 0; i < word.length(); i++) {
-			name[i]+=2;
-			System.out.println(name[i]);
-		}
-		
-		String encryptReadyMessage = JOptionPane.showInputDialog("What would you like to encrypt?");
-		for (int i = 0; i < encryptReadyMessage.length(); i++) {
-			encryptReadyMessage.charAt(i);
-			//switch (i) {
-			//case value:                                                                                                                                                                                                               
-				
-			//	break;
 
-			//default:
-			//	break;
+	static void letsEncryptSomething() {
+		String word = "David";
+		char[] name = word.toCharArray();
+		for (int i = 0; i < word.length(); i++) {
+			name[i] += 2;
+		}
+
+		String encryptReadyMessage = JOptionPane.showInputDialog("What would you like to encrypt?");
+		String otherString = new String();
+		for (int i = 0; i < encryptReadyMessage.length(); i++) {
+			otherString += (int) encryptReadyMessage.charAt(i);
+			System.out.println(otherString);
+			switch (i) {
+			case 1:
+				// break;
+
+				// default:
+				// break;
 			}
 		}
 	}
-
+}
