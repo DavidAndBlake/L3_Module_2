4a54124beddcf364c366bee93d98082fc44e0dc7
NickSanford4 <league@WTS14.attlocal.net>
2018-02-18 17:26:57 -0800

    didn't make much progress

--- src/intro_to_file_io/Encryption.java
+++ src/intro_to_file_io/Encryption.java
@@ -25,28 +25,30 @@ public class Encryption {
 		String otherString = new String();
 		try {
 			FileWriter fw = new FileWriter("src/intro_to_file_io/davidswritingtest.txt");
-			
+
 			/*
-			NOTE: To append to a file that already exists, add true as a second parameter when calling the
-			      FileWriter constructor.
-			      (e.g. FileWriter fw = new FileWriter("src/intro_to_file_io/test2.txt", true);)
-			*/
-			
+			 * NOTE: To append to a file that already exists, add true as a second parameter
+			 * when calling the FileWriter constructor. (e.g. FileWriter fw = new
+			 * FileWriter("src/intro_to_file_io/test2.txt", true);)
+			 */
+
 			fw.write(encryptReadyMessage);
-				
+
 			fw.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		String newVaria = new String();
 		for (int i = 0; i < encryptReadyMessage.length(); i++) {
 			otherString += (int) encryptReadyMessage.charAt(i);
 			System.out.println(otherString);
 			switch (i) {
-			case 1:
-				// break;
+			case 97:
+				i += 3;
+				break;
 
-				// default:
-				// break;
+			// default:
+			// break;
 			}
 		}
 	}
