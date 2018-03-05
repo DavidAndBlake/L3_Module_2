18630626a0e511040edcb16c0d134f48b9b6ab3b
DavidAndBlake <league@WTS13.attlocal.net>
2018-03-04 17:27:31 -0800

    just barely started to make progress

---
+++ src/intro_to_file_io/SavingAFile.java
@@ -0,0 +1,12 @@
+package intro_to_file_io;
+
+import javax.swing.JOptionPane;
+
+public class SavingAFile {
+
+	public static void main(String[]args) {
+
+		String encryptableMessage = JOptionPane.showInputDialog("If you see something, say something.");
+		System.out.println(encryptableMessage);
+	}
+}
